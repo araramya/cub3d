c615051a3bf1337b860591f1ef2fe917afed6f53
Arsen Aramyan <araramya@c1r4s3.42yerevan.am>
2021-04-28 21:48:07 +0400

    9:48

--- getting_startted_ex1.c
+++ getting_startted_ex1.c
@@ -1,5 +1,6 @@
 //ex1 Print squares, circles, triangles and hexagons on the screen by writing the pixels accordingly
 #include <mlx.h>
+#include <math.h>
 
 typedef struct s_data
 {
@@ -32,8 +33,10 @@ int main()
     img.img = mlx_new_image(mlx,1920,1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
     
-    /*
+/*
     //print tringle 
+
+
     //print horizontal
     while(y++ <= 300 )
         my_mlx_pixel_put(&img, x, y, 0x00FF0000);
@@ -46,8 +49,10 @@ int main()
        my_mlx_pixel_put(&img,x,y, 0x00FF0000);
     }
     */
-    /*
+    
     //print empty square 
+
+/*
     while(y <= 300)
         my_mlx_pixel_put(&img, x, ++y, 0x00FF0000);
     while(x <= 300)
@@ -56,26 +61,48 @@ int main()
         my_mlx_pixel_put(&img, x, --y, 0x00FF0000);
     while(x >= 5)
         my_mlx_pixel_put(&img, --x, y, 0x00FF0000);
-    */
-
-   /*
+    
+*/
+/*
    //print full square
+
+
    while(y <= 300)
    {
        x = 4;
-       while(x<= 300)
+       while(x <= 300)
        {
            my_mlx_pixel_put(&img, x, y, 0x00FF0000);
            x++;
        }
        y++;
    }
-
 */
 
+//print circle name (x-h)^2 + (y-k)^2 = r^2
+    int x_c = 960;
+    int y_c = 540; // center
+    my_mlx_pixel_put(&img, x_c, y_c, 0x00FF0000);
 
+    int radius = 200;
+    y = y_c - radius; //puteing xy in the right place to print
+    x = x_c - radius;
+    my_mlx_pixel_put(&img, x, y, 0x0000FF00);
+    int y_cpy = y;
+    int x_cpy = x;
+    while(y <= (y_c + radius)  ) 
+    {
+        while(x <= x_cpy *  2)
+        {
+            if(((x-x_c)*(x-x_c) + (y-y_c)*(y-y_c)) == radius*radius)
+                my_mlx_pixel_put(&img, x, y, 0x0000FF00);
+            x++;
+        }
+        x= x_c -radius;
+        y++;
+    }
+  //  my_mlx_pixel_put(&img, x, y, 0x0000FF00);
+  
     mlx_put_image_to_window(mlx, mlx_win,img.img, 0,0);
-    
     mlx_loop(mlx);
-
 }
